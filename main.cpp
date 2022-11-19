5f5d727b40cf4ed407b8486e3ec9ea9de18e513c
Yusuf Pisan <pisan@uw.edu>
2022-11-19 19:42:25 +0000

    start

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Box {
+public:
+  string name;
+  int count;
+  Box(string name) : name{name}, count{0} {}
+  Box(string name, int count) : name{name}, count{count + 3} {}
+  Box(const Box &b) : name{b.name + "-2"}, count{b.count + 5} {}
+  ~Box() { cout << name << ": " << count << endl; }
+  Box trick(Box b1, Box &b2, int add) {
+    Box bfinal = b1;
+    bfinal.count += add;
+    b2.count += add;
+    return bfinal;
+  }
+};
+
+void test1() {
+  Box ba("ba");
+  Box bb("bb", 10);
+  Box bc(ba);
+  Box bd = ba.trick(bb, bc, 7);
+}
+
+int main() {
+  test1();
+  return 0;
+}
