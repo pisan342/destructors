4a2ee2ea7333bc71534ba6cffb283f1fb2544416
Yusuf Pisan <pisan@uw.edu>
2022-11-22 16:32:09 +0000

    explanations

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/**
+ * Constructor can be called explicitly
+ * Constructor can be called implicitly if it is not defined as explicit
+ *
+ * Copy-constructor can be called explicitly
+ * Copy-constructor called when passed by value
+ * Copy constructor called for = statement when a new variable is initialized
+ *
+ * Copy elision: if a temporary value is going to be assignned to a new
+ *variable, instead of copy-move, compiler must construct the value in place for
+ *the new variable. Copy constructor is *not* called
+ *
+ * Items are allocated on the stack and destructor is called on them
+ * in the opposite order they were created.
+ *
+ **/
+
 #include <iostream>
 
 using namespace std;
@@ -65,10 +82,25 @@ Box get10() {
   return b;
 }
 
+// copy elision - instead of a copy-move operation for the temporary value
+// the value is constructed in-place
 void test3() {
   Box ba("ba");     // call constructor
   Box bb = ba;      // call copy constructor
   Box bc = get10(); // copy elision, copy constructor not called
+}
+
+void displayBoxString(Box b) { cout << "displayBoxString: " << b.name << endl; }
+
+// if the Box constructor with single parameter does not have explicit
+// the below function will compile annd run
+// The compiler use the Box connstructor to implicitly convert a string
+// into a Box object and the calls displayBoxString
+void test4() {
+  Box b("b");
+  displayBoxString(b);
+  string s = "test";
+  displayBoxString(s);
 }
 
 int main() {
@@ -79,4 +111,6 @@ int main() {
   // cout << "test-3" << endl;
   // test3();
   return 0;
+  // cout << "test-4" << endl;
+  // test4();
 }
